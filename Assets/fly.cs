721dc62e375a2befb8e0d016a2cac9edd4119f15
brokkoli71 <brokkoli71b@gmail.com>
2021-04-26 22:24:51 +0200

    add fly

--- Assets/fly.cs
+++ Assets/fly.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class fly : MonoBehaviour
 {
+    private Rigidbody playerRb;
+    
+    public float speed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(playerRb.rotation.ToEulerAngles() * forwardInput * speed);
     }
 }
